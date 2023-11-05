feadfa6c82941bb2ae405435d8d25608413f2552
monisha-max <115718081+monisha-max@users.noreply.github.com>
2023-11-05 12:15:35 +0530

    Update summarised_txt.py

--- data_summarization/summarised_txt.py
+++ data_summarization/summarised_txt.py
@@ -28,13 +28,20 @@ def get_summarised_txt(df, columns: list = [], autodetect: bool = True) -> list:
 
     if autodetect:
         columns = auto_select_coloumn(df.columns.to_list())
-
-    for i in columns:
-        plot_graph2(df, i[0], i[1])
+    
+    #columns = ['Education', 'Joining Year', 'City', 'Paymenteir', 'Age', 'Gender', 'EverBenched']
+    columns = ['Education','JoiningYear','City','PaymentTier','Age','Gender','EverBenched','ExperienceInCurrentDomain','LeaveOrNot']
+    
+    
+    plot_graph2(df, columns[2], columns[3])
+    #plot_graph2(df, columns[6], columns[7])
+   
+    #for i in columns:
+    #    plot_graph2(df, i[0], i[1])
         #TODO: figure out the inputs and outputs of the graphing part
         #Returning the graphs as png
     
-
+    columns = auto_select_coloumn(df.columns.to_list())
     outputs = list()
     for i in columns:
         ip = create_input_str(i, df)
